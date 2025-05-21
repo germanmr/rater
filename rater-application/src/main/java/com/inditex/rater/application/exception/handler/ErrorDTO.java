564cffb4731d026f8b36c4f988d7d1c347bd47b1
German Muñoz Romano <german.munioz.romano@gmail.com>
2025-05-21 21:34:23 +0200

    feat: Record class

--- rater-application/src/main/java/com/inditex/rater/application/exception/handler/ErrorDTO.java
+++ rater-application/src/main/java/com/inditex/rater/application/exception/handler/ErrorDTO.java
@@ -1,13 +1,7 @@
 package com.inditex.rater.application.exception.handler;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 
-@AllArgsConstructor
 @Builder
-@Data
-public final class ErrorDTO {
-    private final String code;
-    private final String message;
+public record ErrorDTO(String code, String message) {
 }
